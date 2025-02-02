ed3dfd3a909d47da7add527362c818ac7964f8e2
lquifi <55291238+lquifii@users.noreply.github.com>
2025-02-02 14:55:43 +0100

    added disabler test - croat

--- src/main/java/keystrokesmod/module/impl/world/Disabler.java
+++ src/main/java/keystrokesmod/module/impl/world/Disabler.java
@@ -1,13 +1,27 @@
 package keystrokesmod.module.impl.world;
 
+import keystrokesmod.event.ReceivePacketEvent;
+import keystrokesmod.event.SendPacketEvent;
 import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.SliderSetting;
+import keystrokesmod.utility.Utils;
 import net.minecraft.network.play.client.C0BPacketEntityAction;
 import net.minecraft.network.play.client.C0BPacketEntityAction.Action;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.network.play.server.S07PacketRespawn;
+import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 
 public class Disabler extends Module {
+
+    boolean isJumping = false;
+    boolean isDisabling = false;
+    int tickCounter = 0;
+    public boolean isDisabled = false;
+    int lastTickTime = 0;
+    boolean isInLobby = false;
+
     private SliderSetting mode;
-    private String[] modes = new String[]{"Meow", "Full", "00", "SprintCancel"};
+    private String[] modes = new String[]{"Meow", "Full", "00", "SprintCancel", "MotionDisabler"};
 
     public Disabler() {
         super("Disabler", Module.category.world);
@@ -23,6 +37,39 @@ public class Disabler extends Module {
     public void onUpdate() {
         if (modes[(int) mode.getInput()].equals("SprintCancel")) {
             mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, Action.STOP_SPRINTING));
+        }
+    }
+
+    public void packet(ReceivePacketEvent event) {
+        if (modes[(int) mode.getInput()].equals("MotionDisabler")) {
+            if (!this.isEnabled()) {
+                return;
+            }
+            if (event.getPacket() instanceof S02PacketChat) {
+                if (((S02PacketChat) event.getPacket()).getChatComponent().getUnformattedText().toLowerCase().contains("joined the lobby")) {
+                    isInLobby = true;
+                }
+            }
+
+            if (event.getPacket() instanceof S07PacketRespawn) {
+                isJumping = true;
+            }
+
+            if (event.getPacket() instanceof S08PacketPlayerPosLook) {
+                tickCounter++;
+                if (tickCounter == 23) {
+                    isDisabling = false;
+                    tickCounter = 0;
+                    lastTickTime = mc.thePlayer.ticksExisted - lastTickTime;
+                    Utils.sendMessage("Disabling Watchdog");
+                    if (tickCounter == 90) {
+                        Utils.sendMessage("Disabled jump checks");
+                        isDisabled = true;
+                    } else if (tickCounter < 23) {
+                        mc.thePlayer.motionY = mc.thePlayer.motionZ = mc.thePlayer.motionX = 0;
+                    }
+                }
+            }
         }
     }
 }
